292f1d2a6ae621e2689f061433bf47f923753aa2
SooYeon Kim <sooyeon0809@gmail.com>
2020-10-26 00:02:27 +0900

    added products API to get a list of products
    get a list of products when call products api
    search by each category

--- src/main/kotlin/com/sooyeon/sandramall/controller/ProductApiController.kt
+++ src/main/kotlin/com/sooyeon/sandramall/controller/ProductApiController.kt
@@ -1,21 +1,22 @@
 package com.sooyeon.sandramall.controller
 
 import com.sooyeon.sandramall.ApiResponse
+import com.sooyeon.sandramall.domain.product.Product
+import com.sooyeon.sandramall.domain.product.ProductService
 import com.sooyeon.sandramall.domain.product.registration.ProductImageService
 import com.sooyeon.sandramall.domain.product.registration.ProductRegistrationRequest
 import com.sooyeon.sandramall.domain.product.registration.ProductRegistrationService
+import com.sooyeon.sandramall.domain.product.toProductListItemResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api/v1")
 class ProductApiController @Autowired constructor(
     private val productImageService: ProductImageService,
-    private val productRegistration: ProductRegistrationService
+    private val productRegistration: ProductRegistrationService,
+    private val productService: ProductService
 ) {
     @PostMapping("/product_images")
     fun uploadImage(image: MultipartFile) =
@@ -27,4 +28,16 @@ class ProductApiController @Autowired constructor(
     fun register(
         @RequestBody request: ProductRegistrationRequest
     ) = ApiResponse.ok(productRegistration.register(request))
+
+    @GetMapping("/products")
+    fun search(
+        @RequestParam productId: Long,
+        @RequestParam(required=false) categoryId: Int?,
+        @RequestParam direction: String,
+        @RequestParam(required = false) limit: Int?
+    ) = productService
+        .search(categoryId, productId, direction, limit ?: 10)
+        .mapNotNull(Product::toProductListItemResponse)
+        .let { ApiResponse.ok(it) }
+
 }
